88d30ad26ea6a914a3100ae0aa2d69a8e2f9fae7
developerNd <91623104+developerNd@users.noreply.github.com>
2025-08-18 23:22:05 +0530

    config updated

--- client/src/lib/config.ts
+++ client/src/lib/config.ts
@@ -3,7 +3,7 @@ export const API_CONFIG = {
   // Use production API URL
   get API_URL() {
     return 'https://api.localsoch.com';
-    // return 'http://192.168.1.100:1337';
+    // return "http://192.168.150.101:1337";
   },
   
   // Get the appropriate image URL based on environment
